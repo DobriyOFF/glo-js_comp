45f058ccbe191e28332ce7e0df5ad870ff5bf348
Sergey <segaoff@icloud.com>
2022-02-02 22:48:02 +0300

    изменил второе условие

--- lesson4/main.js
+++ lesson4/main.js
@@ -1,14 +1,16 @@
 const foo = (str) => {
-  if (typeof str !== "string")
+  if (typeof str !== "string") {
     console.log("тут не строка");
+  }
   else {
     str = str.trim();
-    console.log(str);
+    if (str.length > 30){
+      str = str.substring(0, 30) + "...";
+      console.log(str);
+    } else {
+      console.log(str);
+    }
   }
-  if (str.length > 30){
-    str = str.substring(0, 30) + "...";
-    console.log(str);
-  } 
 }
 
-foo("   123")
+foo("   123asdfkasdflasdkfjlskfjd;als")
